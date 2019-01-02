eb335a2793aaa28c31cb62223ef1641fbccf876a
chanmol1999 <chanmol1999@gmail.com>
2019-01-02 15:57:17 +0530

    Added a welcome screen

---
+++ app/src/main/java/gis2018/udacity/tametu/first.java
@@ -0,0 +1,43 @@
+package gis2018.udacity.tametu;
+
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class first extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        TextView textv = (TextView) findViewById(R.id.tametu);
+        textv.setShadowLayer(85, 0, 0, Color.WHITE);
+        /** Called when the activity is first created. */
+
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(first.this, MainActivity.class);
+                first.this.startActivity(mainIntent);
+                first.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+
+    }
+}
