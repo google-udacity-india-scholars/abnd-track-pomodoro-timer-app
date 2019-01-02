44e8a6964a1585b72567d382714502501b9b5279
chanmol1999 <chanmol1999@gmail.com>
2019-01-02 16:57:46 +0530

    Updated welcome with required changes

--- app/src/main/java/gis2018/udacity/tametu/first.java
+++ app/src/main/java/gis2018/udacity/tametu/first.java
@@ -1,20 +1,15 @@
 package gis2018.udacity.tametu;
 
 
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.view.animation.Animation;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class first extends AppCompatActivity {
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    private final int SPLASH_DISPLAY_LENGTH = 1200;
 
 
     @Override
