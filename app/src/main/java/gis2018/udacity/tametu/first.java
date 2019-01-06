9f1ab49dbbbffb41f71d07db6c7ebb660d9227a4
iamhshaikh <hannanshaikh33@gmail.com>
2019-01-06 12:32:04 +0530

    Redesign Welcome screen

--- app/src/main/java/gis2018/udacity/tametu/first.java
+++ app/src/main/java/gis2018/udacity/tametu/first.java
@@ -2,11 +2,9 @@ package gis2018.udacity.tametu;
 
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
 public class first extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 1200;
@@ -16,9 +14,7 @@ public class first extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
-        TextView textv = (TextView) findViewById(R.id.tametu);
-        textv.setShadowLayer(85, 0, 0, Color.WHITE);
-        /** Called when the activity is first created. */
+        /** Called when the activity is first created. **/
 
 
         /* New Handler to start the Menu-Activity
@@ -28,8 +24,7 @@ public class first extends AppCompatActivity {
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(first.this, MainActivity.class);
-                first.this.startActivity(mainIntent);
-                first.this.finish();
+                startActivity(mainIntent);
             }
         }, SPLASH_DISPLAY_LENGTH);
 
